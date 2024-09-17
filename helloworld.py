02c57c652080d8e2e4920e168967911b8e017f7f
pandaman109010 <jaden.meharry@kingsway.school.nz>
2024-09-18 10:25:17 +1200

    Add files via upload
    first lesson
    
    hello

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+#Author: https://www.github.com/pandaman109010
+#Date: 18/09/24
+#Version: 0.1
+
+
+#this is a comment
+hi = "hello world"#this is a verable
+print(hi+"\n")#\n creates a line break
+print()#this creates a line break
+print("yay")
