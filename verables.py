a174228372ef039ee64afacc9f73422b3329bd11
pandaman109010 <jaden.meharry@kingsway.school.nz>
2024-09-20 14:36:17 +1200

    Add files via upload
    different types of variables

---
+++ verables.py
@@ -0,0 +1,21 @@
+#verables
+#author: panda https://github.com/pandaman109010
+#date: 20/09/24
+#v1
+
+#different types of variables
+#store an int
+num_1 = 15
+#store a float
+num_1 = 15.5
+print(num_1)
+
+#store a string
+name = "panda"
+print(name)
+
+#do calcs in variable and store the result
+sum = 5 + 5 #fallows pep8 python conventions
+print(sum)
+
+print(name + sum)
