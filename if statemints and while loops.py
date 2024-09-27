8d54a6a3bc11ee841beeb1a62dd25297db9112e3
Jaden Meharry <jaden.meharry@kingsway.school.nz>
2024-09-27 14:58:28 +1200

    Add files via upload
    dumb chat bot

---
+++ if statemints and while loops.py
@@ -0,0 +1,35 @@
+# conditional statements and while loops
+# author : panda, https://github.com/pandaman109010
+# date: 2024/09/27
+# V1
+# TODO: create a program that asks a user a question 
+# and returns a response based on the answer of the user.
+
+
+#main loop. keeps running until a condition is met
+main = ""
+while main == "":
+
+    like_coffee = input("do you like coffee ").lower()
+    #.lower convirts evry text to lowercase
+
+    if like_coffee == "yes" or like_coffee == "yip" or like_coffee == "YES" or like_coffee == "Y" or like_coffee == "y":
+        print("same")
+        main == input("press <enter> to ask agan or anything else to quit ")
+    elif like_coffee == "no" or like_coffee == "n" or like_coffee == "nah":
+        print(like_coffee)
+        print("you suck")
+
+        like_tea = input("do you like tea instead ").upper()
+        if like_tea == "yes":
+            print("same")
+        elif like_tea == "NO" or like_tea == "N":
+            print("brh")
+            main == input("press <enter> to ask agan or anything else to quit ")
+
+        else:
+            print(like_tea)
+            print("brh give me a proper yes or no")
+    else:
+        print("brh give me a proper yes or no")
+        
